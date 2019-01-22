aa9ec9422911ffc3c67bc0dbb875dee73c342154
zxz <690792862@qq.com>
2019-01-22 17:08:31 +0800

    首页完成

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-$(function() {
-	$('.zfbmwj .top ul li').on('mouseover', function() {
+$(function () {
+	$('.lbt_news .top ul li').on('mouseover', function () {
 		$(this).addClass("now").siblings().removeClass("now");
-		$('.zfbmwj .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+		$('.lbt_news .top > a').attr('href', $(this).children().attr('href'))
+		$('.lbt_news .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
 	});
-	// $('.zxxxgk .top ul li').on('mouseover', function() {
-	// 	$(this).addClass("now").siblings().removeClass("now");
-	// 	$('.zxxxgk .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
-	// });
+	lbt_wz(".lbt .img li", ".lbt .img", ".lbt .num", ".lbt .num li", ".lbt", "now", 617, ".lbt .hwz > div");
+	jQuery(".picScroll-left").slide({
+		titCell: ".hd ul",
+		mainCell: ".bd ul",
+		autoPage: true,
+		effect: "left",
+		autoPlay: true,
+		scroll: 5,
+		vis: 5,
+		delayTime: 1000
+	});
+	$('.box3 .left ul li').on('click',function(){
+		$(this).addClass("now").siblings().removeClass("now");
+		$('.box3 .right > div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+	})
+	$('.zcjd .top ul li').on('mouseover', function () {
+		$(this).addClass("now").siblings().removeClass("now");
+		$('.zcjd .top > a').attr('href', $(this).children().attr('href'))
+		$('.zcjd .bottom>div').eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+	});
 })
